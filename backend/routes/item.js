9d9e133643c53a34a454eda0987d24df93756eb1
David Poza Suarez <pozasuarez@gmail.com>
2018-06-25 12:53:33 +0200

    protegemos con login todos los endpoint de items

--- backend/routes/item.js
+++ backend/routes/item.js
@@ -8,10 +8,13 @@ var router = express.Router();
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart({ uploadDir: './uploads' });
 
-router.get('/items', ItemController.getItems);
-router.get('/items/:id', ItemController.getItem);
-router.put('/items/:id', ItemController.updateItem);
-router.delete('/items/:id', ItemController.deleteItem);
-router.post('/items', ItemController.addItem);
+var md_auth = require('../middleware/authenticated');
+
+
+router.get('/items', md_auth.ensureAuth, ItemController.getItems);
+router.get('/items/:id', md_auth.ensureAuth, ItemController.getItem);
+router.put('/items/:id', md_auth.ensureAuth, ItemController.updateItem);
+router.delete('/items/:id', md_auth.ensureAuth, ItemController.deleteItem);
+router.post('/items', md_auth.ensureAuth, ItemController.addItem);
 
 module.exports = router;

3a1abb9f2a3ec67bac0cb17463a1e95a2a2be412
David Poza Suarez <pozasuarez@gmail.com>
2018-06-29 16:11:08 +0200

    boton volver atras en vista de lista

--- frontend/src/app/components/items/items.component.ts
+++ frontend/src/app/components/items/items.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Item } from '../../../models/item';
 import { ListService } from '../../../services/list.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-items',
@@ -11,11 +12,13 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 })
 export class ItemsComponent implements OnInit {
   public items: Array<Item>;
+  public listName: String;
 
   constructor(
     private _listService: ListService,
     private _router: Router,
-    private _route: ActivatedRoute
+    private _route: ActivatedRoute,
+    private location: Location
   ) {
     //this.items = [
     //  new Item('','Pollo', 1, 1, 'bandeja',''), 
@@ -31,6 +34,7 @@ export class ItemsComponent implements OnInit {
     this._route.params.subscribe(params => {
       let id = params.id;
       this.getItems(id);
+      this.getList(id);
     })
     
   }
@@ -47,4 +51,21 @@ export class ItemsComponent implements OnInit {
       }
     );
   }
+
+  getList(id){
+    this._listService.getList(id).subscribe(
+      response =>{
+        console.log(response);
+        this.listName = response.list.name;
+        
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  goBack() {
+    this.location.back();
+}
 }

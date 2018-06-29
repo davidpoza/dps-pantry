5e242c91d785cc9e744b87061d3226f07d611f12
David Poza Suarez <pozasuarez@gmail.com>
2018-06-29 13:26:56 +0200

    ejemplo de items hardcoded

--- frontend/src/app/components/items/items.component.ts
+++ frontend/src/app/components/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../../../models/item';
 
 @Component({
   selector: 'app-items',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
+  public items: Array<Item>;
 
-  constructor() { }
+  constructor() {
+    this.items = [
+      new Item('','Pollo', 1, 1, 'bandeja',''), 
+      new Item('','Leche de almendras', 3, 1, 'bricks',''),
+      new Item('','Canonigos', 1, 1, 'bolsa',''),
+      new Item('','Cereales sin gluten', 1, 1, 'paquete',''),
+      new Item('','Edulcorante', 1, 1, 'bote',''),          
+      ];
+   }
 
   ngOnInit() {
   }
 
+  getItems(){
+    
+  }
 }
---
+++ frontend/src/models/item.ts
@@ -0,0 +1,12 @@
+export class Item{
+    constructor(
+        public _id: string,
+        public name: string,
+        public quantity: number,
+        public minimum: number,
+        public unit: string,
+        public notes: string
+    ){
+
+    }
+}

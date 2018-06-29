b0074eabd1ec98ee872db1180da1a6958d443491
David Poza Suarez <pozasuarez@gmail.com>
2018-06-29 13:03:28 +0200

    componente items, que es un listado de items de una lista concreta

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -2,9 +2,12 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ListsComponent } from './components/lists/lists.component';
+import { ItemsComponent } from './components/items/items.component';
 
 const appRoutes: Routes = [
     { path: '', component: ListsComponent},
+    { path: 'list/:id', component: ItemsComponent}
+
 ];
 
 export const appRoutingProviders: any[] = [];

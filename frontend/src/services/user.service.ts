03e5872894664218674ab21e7892d11119a72d73
David Poza Suarez <pozasuarez@gmail.com>
2018-07-02 17:13:05 +0200

    métodos para obtener la identidad y el token del localStorage.

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Global } from './global';
+import { User } from '../models/user';
 
 @Injectable()
 export class UserService{
     public url:string;
+    public identity;
+    public token;
 
     constructor(
         private _http: HttpClient
@@ -17,5 +20,35 @@ export class UserService{
         return 'Probando';
     }
 
+    login(user: User, gettoken): Observable<any>{
+        if(gettoken){
+            user.gettoken = gettoken;
+        }
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        let params = JSON.stringify(user);
+        return this._http.post(this.url+'login/', params, {headers:headers});
+    }
+
+    getIdentity(){
+        let identity = JSON.parse(localStorage.getItem('identity'));
+        if(identity != "undefined"){
+            this.identity = identity;
+        }
+        else{
+            this.identity = null;
+        }
+        return this.identity;
+    }
+
+    getToken(){
+        let token = JSON.parse(localStorage.getItem('token'));
+        if(token != "undefined"){
+            this.token = token;
+        }
+        else{
+            this.token = null;
+        }
+        return this.token;
+    }
 
 }

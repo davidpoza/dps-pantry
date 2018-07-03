0cee5f1d6368f3556e5d6d0b32ef0f808eb9d784
David Poza Suarez <pozasuarez@gmail.com>
2018-07-03 11:18:09 +0200

    obligamos a enviar la cabecera de authorization en todas las peticiones.

--- backend/routes/item.js
+++ backend/routes/item.js
@@ -11,9 +11,9 @@ var multipartMiddleware = multipart({ uploadDir: './uploads' });
 var md_auth = require('../middleware/authenticated');
 
 
-router.get('/items', ItemController.getItems);
-router.get('/listitems/:id', ItemController.getListItems);
-router.get('/items/:id', ItemController.getItem);
+router.get('/items', md_auth.ensureAuth, ItemController.getItems);
+router.get('/listitems/:id', md_auth.ensureAuth, ItemController.getListItems);
+router.get('/items/:id', md_auth.ensureAuth, ItemController.getItem);
 router.put('/items/:id', md_auth.ensureAuth, ItemController.updateItem);
 router.delete('/items/:id', md_auth.ensureAuth, ItemController.deleteItem);
 router.post('/items', md_auth.ensureAuth, ItemController.addItem);

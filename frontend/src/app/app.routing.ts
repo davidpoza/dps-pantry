a32b02361092cb3d7560fd292cc2dca433dbe03a
David Poza Suarez <pozasuarez@gmail.com>
2018-07-04 12:59:03 +0200

    nuevo componente para formulario añadir lista

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ListsComponent } from './components/lists/lists.component';
 import { ItemsComponent } from './components/items/items.component';
 import { LoginComponent } from './components/login/login.component';
+import { AddListFormComponent } from './components/add-list-form/add-list-form.component';
 
 const appRoutes: Routes = [
     { path: '', component: ListsComponent},
     { path: 'list/:id', component: ItemsComponent},
     { path: 'login', component: LoginComponent},
+    { path: 'addlist', component: AddListFormComponent},
 
 ];
 
